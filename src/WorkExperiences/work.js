870501fcb9143e0e488bb47ea15cb79d5507ba40
izzuan96 <izzuanzawawi@gmail.com>
2024-11-05 06:25:52 +0800

    update on the image

--- src/WorkExperiences/work.js
+++ src/WorkExperiences/work.js
@@ -12,24 +12,28 @@ const workExperiences = [
     company: 'Accenture',
     logo: AccentureIcon,
     description: 'I’m like the internet’s peacekeeper – filtering out negativity and keeping the digital neighborhood safe. Think of me as the guardian of good vibes, stepping in to ensure everyone can scroll through a drama-free zone. It’s a tough job, but someone’s got to keep the online world a little kinder!',
+    customClass: 'accenture-class', // Unique class for Accenture
   },
   {
     id: 2,
     company: 'Maybank',
     logo: MaybankIcon,
     description: 'I was the front-end wizard at Maybank, turning complex investment projects into sleek, user-friendly experiences. My mission? Making finance look so smooth, you’d forget it’s all about numbers!',
+    customClass: 'maybank-class', // Unique class for Maybank
   },
   {
     id: 3,
     company: 'Angkatan Pertahanan Awam Malaysia (APM)',
     logo: ApmIcon,
     description: 'During COVID, I worked with Angkatan Pertahanan Awam Malaysia, overseeing Person under investigation (PUI) from KLIA and various hotels in Kuala Lumpur. I coordinated with key agencies like KKM and PDRM to ensure everything ran smoothly. Think of it as managing a high-stakes operation where every detail was crucial!',
+    customClass: 'apm-class', // Unique class for APM
   },
   {
     id: 4,
     company: 'Schlumberger',
     logo: SlbIcon,
     description: 'I started as a Schlumberger intern in the Service Help Desk, supporting global staff with Schlumberger’s online systems. Whether it was office tech glitches or system issues on oil rigs, I was the one ensuring the digital side stayed smooth and seamless – basically, the IT troubleshooter for all things digital!',
+    customClass: 'schlumberger-class', // Unique class for Schlumberger
   },
 ];
 
@@ -37,11 +41,11 @@ function Work() {
   return (
     <>
       <div className='work-wrap'>
-        <Title text="Career Milestones" className="workList-title" color ="#ffff" />
+        <Title text="Career Milestones" className="workList-title" color="#ffff" />
         <div className='workList'>
           {workExperiences.map(experience => (
             <div key={experience.id} className='workItem'>
-              <div className='imgcontainer'>
+              <div className={`imgcontainer ${experience.customClass}`}>
                 <img src={experience.logo} alt={`${experience.company} logo`} />
               </div>
               <div className='description'>
